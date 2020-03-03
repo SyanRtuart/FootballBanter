5e6be8870bbbcc34e17ae964baa27b660d460539
Ryan Stuart <rstuart@mackays.co.uk>
2020-03-03 13:54:23 +0000

    adding phrases and results service

---
+++ Services/Results/Results.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Results.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ Services/Results/Results.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Results.Domain
+{
+    public class Class1
+    {
+    }
+}
