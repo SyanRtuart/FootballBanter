6315894f14e1c8088d2a7ec33caafac8cf0df038
Ryan Stuart <ryan_7229@hotmail.co.uk>
2021-04-01 17:53:35 +0100

    added createdByUserId to CreatePhraseRequest

--- ApiGateways/Web.Bff/Web.HttpAggregator/Models/Phrase/CreatePhraseRequest.cs
+++ ApiGateways/Web.Bff/Web.HttpAggregator/Models/Phrase/CreatePhraseRequest.cs
@@ -6,6 +6,7 @@ namespace Web.HttpAggregator.Models.Phrase
     {
         public Guid MatchId { get; set; }
         public Guid TeamId { get; set; }
+        public Guid CreatedByUserId { get; set; }
         public string Description { get; set; }
         public bool Positive { get; set; }
     }
